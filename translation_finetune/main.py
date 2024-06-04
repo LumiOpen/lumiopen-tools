ddde8198cf2437e2c973f0cd30dbd6b50278a4a4
Joona Kytöniemi <jnkyto@utu.fi>
2024-06-04 15:28:14 +0300

    make tokenization work

--- translation_finetune/main.py
+++ translation_finetune/main.py
@@ -28,21 +28,18 @@ def argparser():
     ap.add_argument('--verbose', action='store_true')
     ap.add_argument('--max-length', type=int, default=1024)
     ap.add_argument("--batch_size", "-b", type=int, default=16)
+    ap.add_argument("--dry_run", "-d", action="store_true")
     ap.add_argument('--model', default=DEFAULT_MODEL)
     return ap
 
 
-def prepper(data):
+def prepper(translations):
     template = "<|user|>Käännä suomeksi: {} <|assistant|>"
 
-    formatted_data = []
-    for idx, entry in enumerate(data["translation"]):
-        formatted_en = template.format(entry["en"])
-        response = entry["fi"]
-        final = f"{formatted_en}{response}"
-        formatted_data.append(final)
+    for entry in translations["translation"]:
+        entry["en"] = template.format(entry["en"])
 
-    return formatted_data
+    return translations
 
 
 def main(argv):
@@ -50,23 +47,30 @@ def main(argv):
     args = argparser().parse_args(argv[1:])
     tokenizer = AutoTokenizer.from_pretrained(args.model)
 
-    ds = load_dataset("Helsinki-NLP/europarl", "en-fi", split="train") # With europarl, everything's in "train"
+    ds = load_dataset("Helsinki-NLP/europarl", "en-fi", split="train")  # With europarl, everything's in "train"
     ds = ds.shuffle(random.seed(5834)).select(range(10000))  # Shuffle dataset and limit sample amount
     ds = ds.train_test_split(test_size=0.2)
 
-    def tokenize(example):
-        return tokenizer(
-            example,
-            max_length=args.max_length,
-            truncation=True,
-        )
+    def tokenize(translations):
+        for entry in translations["translation"]:
+            entry["en"] = tokenizer(
+                entry["en"],
+                max_length=args.max_length,
+                truncation=True
+            )
 
-    def preprocess(dataset):
-        data_train = prepper(data=ds["train"])
-        data_test = prepper(data=ds["test"])
-        data_train_tokenized = list(map(tokenize, data_train))
-        data_test_tokenized = list(map(tokenize, data_test))
-        return {0: data_train_tokenized, 1: data_test_tokenized}
+            entry["fi"] = tokenizer(
+                entry["fi"],
+                max_length=args.max_length,
+                truncation=True
+            )
+
+        return translations
+
+    def preprocess(translations):
+        prepped_translations = prepper(translations)
+        tokenized_translations = tokenize(prepped_translations)
+        return tokenized_translations
 
     # print(data["train"][0])
     # print(data["test"][0])
@@ -74,76 +78,80 @@ def main(argv):
     # print(f"{type(data_test_tokenized)}: {data_test_tokenized[0]}")
 
     with accelerator.main_process_first():
-        tokenized_datasets = ds.map(
+        data_train_tokenized = ds["train"].map(
             preprocess,
             batched=True,
             load_from_cache_file=False,
-            desc="Dataset tokenization process has started."
+        )
+        data_test_tokenized = ds["test"].map(
+            preprocess,
+            batched=True,
+            load_from_cache_file=False,
         )
 
-    data_train_tokenized = tokenized_datasets[0]
-    data_test_tokenized = tokenized_datasets[1]
+    if not args.dry_run:
+        model = AutoModelForCausalLM.from_pretrained(
+            args.model,
+            torch_dtype="auto",
+        )
 
-    model = AutoModelForCausalLM.from_pretrained(
-        args.model,
-        torch_dtype="auto",
-    )
+        collator = DataCollatorForLanguageModeling(
+            tokenizer=tokenizer,
+            return_tensors='pt',
+            mlm=False,
+        )
 
-    collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        return_tensors='pt',
-        mlm=False,
-    )
+        train_dataloader = DataLoader(
+            data_train_tokenized, collate_fn=collator, batch_size=args.batch_size, pin_memory=True
+        )
 
-    train_dataloader = DataLoader(
-        data_train_tokenized, collate_fn=collator, batch_size=args.batch_size, pin_memory=True
-    )
+        test_dataloader = DataLoader(
+            data_test_tokenized, collate_fn=collator, batch_size=args.batch_size, pin_memory=True
+        )
 
-    test_dataloader = DataLoader(
-        data_test_tokenized, collate_fn=collator, batch_size=args.batch_size, pin_memory=True
-    )
+        lr = 3e-5
+        num_epochs = 3
+        gradient_accumulation_steps = 8
 
-    lr = 3e-5
-    num_epochs = 3
-    gradient_accumulation_steps = 8
+        optimizer = AdamW(model.parameters(), lr=lr)
+        lr_scheduler = get_linear_schedule_with_warmup(
+            optimizer=optimizer,
+            num_warmup_steps=0,
+            num_training_steps=(len(train_dataloader) * num_epochs)
+        )
 
-    optimizer = AdamW(model.parameters(), lr=lr)
-    lr_scheduler = get_linear_schedule_with_warmup(
-        optimizer=optimizer,
-        num_warmup_steps=0,
-        num_training_steps=(len(train_dataloader) * num_epochs)
-    )
+        model, train_dataloader, test_dataloader, optimizer, lr_scheduler = accelerator.prepare(
+            model, train_dataloader, test_dataloader, optimizer, lr_scheduler
+        )
 
-    model, train_dataloader, test_dataloader, optimizer, lr_scheduler = accelerator.prepare(
-        model, train_dataloader, test_dataloader, optimizer, lr_scheduler
-    )
-
-    for epoch in range(num_epochs):
-        model.train()
-        total_loss = 0
-        for step, batch in enumerate(tqdm(train_dataloader)):
-            outputs = model(**batch)
-            loss = outputs.loss
-            total_loss += loss.detach().float()
-            accelerator.backward(loss)
-
-            if step % gradient_accumulation_steps == 0:
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-                model.zero_grad()
-
-            # capture batch analytics
-
-        model.eval()
-        eval_loss = 0
-        for step, batch in enumerate(tqdm(test_dataloader)):
-            with torch.no_grad():
+        for epoch in range(num_epochs):
+            model.train()
+            total_loss = 0
+            for step, batch in enumerate(tqdm(train_dataloader)):
                 outputs = model(**batch)
-            loss = outputs.loss
-            eval_loss += loss.detach().float()
+                loss = outputs.loss
+                total_loss += loss.detach().float()
+                accelerator.backward(loss)
 
-        model.save_pretrained(f"trained_model-{epoch}")
+                if step % gradient_accumulation_steps == 0:
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+                    model.zero_grad()
+
+                # capture batch analytics
+
+            model.eval()
+            eval_loss = 0
+            for step, batch in enumerate(tqdm(test_dataloader)):
+                with torch.no_grad():
+                    outputs = model(**batch)
+                loss = outputs.loss
+                eval_loss += loss.detach().float()
+
+            model.save_pretrained(f"trained_model-{epoch}")
+        else:
+            print("Skip training because dry run is toggled.")
 
 
 if __name__ == '__main__':
